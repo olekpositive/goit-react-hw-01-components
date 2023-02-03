58cfa25370f092a083680ba19bf8330e294dc559
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2023-02-03 12:31:18 +0100

    Update_0.2_Profile

---
+++ src/components/Profile/Profile-styled.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const ProfileCard = styled.div`
+
+`;
+export const Description = styled.div`
+
+`;
+export const Avatar = styled.div`
+
+`;
+export const Name = styled.div`
+
+`;
+
+export const Tag = styled.div`
+
+`;
+
+export const Location = styled.div`
+
+`;
+export const StatsList = styled.div`
+
+`;
+export const StatsItem = styled.div`
+
+`;
+export const  Label = styled.div`
+
+`;
+export const Quantity = styled.div`
+
+`;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  ProfileCard,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  StatsList,
+  StatsItem,
+  Label,
+  Quantity,
+} from './Profile-styled';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <ProfileCard>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <StatsList>
+        <StatsItem>
+          <Label>followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
+
+        <StatsItem>
+          <Label>views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </StatsList>
+    </ProfileCard>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
